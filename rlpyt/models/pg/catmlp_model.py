b8d76014894297914edb602bb56fc5ffe3c315cd
Willem van Doesburg <wvandoesburg@gmail.com>
2020-05-25 21:53:24 +0200

    Added Advantage Actor Critic version of Deep Sea Treasure Hunt

---
+++ rlpyt/models/pg/catmlp_model.py
@@ -0,0 +1,60 @@
+
+import torch
+from rlpyt.utils.tensor import infer_leading_dims, restore_leading_dims
+import torch.nn.functional as F
+
+class CatMlpPgModel(torch.nn.Module):
+    """Multilayer Perceptron with last layer linear.
+
+    Args:
+        input_size (int): number of inputs
+        hidden_sizes (list): can be empty list for none (linear model).
+        output_size: linear layer at output, or if ``None``, the last hidden size will be the output size and will have nonlinearity applied
+        nonlinearity: torch nonlinearity Module (not Functional).
+    """
+
+    def __init__(
+            self,
+            input_size,
+            hidden_sizes,  # Can be empty list for none.
+            output_size=None,  # if None, last layer has nonlinearity applied.
+            nonlinearity=torch.nn.ReLU,  # Module, not Functional.
+            n_atoms=5
+            ):
+        super().__init__()
+        if isinstance(hidden_sizes, int):
+            hidden_sizes = [hidden_sizes]
+        hidden_layers = [torch.nn.Linear(n_in, n_out) for n_in, n_out in
+            zip([input_size] + hidden_sizes[:-1], hidden_sizes)]
+        sequence = list()
+        for layer in hidden_layers:
+            sequence.extend([layer, nonlinearity()])
+        if output_size is not None:
+            last_size = hidden_sizes[-1] if hidden_sizes else input_size
+            sequence.append(torch.nn.Linear(last_size, output_size))
+        self.model = torch.nn.Sequential(*sequence)
+        self._output_size = (hidden_sizes[-1] if output_size is None
+            else output_size)
+        self._n_atoms = n_atoms
+        self.pi = torch.nn.Linear(self._output_size, output_size)
+        self.value = torch.nn.Linear(self._output_size, 1)
+
+    def forward(self, observation, prev_action, prev_reward):
+        """ Compute the model on the input, assuming input shape [B,input_size].
+            return the probability and values of actions
+        """
+        # Infer (presence of) leading dimensions: [T,B], [B], or [].
+        # For single valued observations the dim argument to infer_leading_dims should be 0
+        lead_dim, T, B, img_shape = infer_leading_dims(observation, 1)
+
+        p = self.model(observation.view(T * B, -1).float())
+        p = restore_leading_dims(p, lead_dim, T, B)
+        pi = F.softmax(self.pi(p), dim=-1)
+        v = self.value(p).squeeze(-1)
+
+        return pi, v
+
+    @property
+    def output_size(self):
+        """Retuns the output size of the model."""
+        return self._output_size
---
+++ rlpyt_testing/rplyt_deep_sea_treasure_a2c.py
@@ -0,0 +1,58 @@
+from rlpyt.samplers.serial.sampler import SerialSampler
+from rlpyt.samplers.parallel.cpu.sampler import CpuSampler
+from rlpyt.envs.gym import GymEnvWrapper
+from rlpyt.models.pg.catmlp_model import CatMlpPgModel
+from rlpyt.runners.minibatch_rl import MinibatchRlEval
+from rlpyt.utils.logging.context import logger_context
+from deep_sea_treasure_env.deep_sea_treasure_env import DeepSeaTreasureEnv
+from collections import namedtuple
+from rlpyt.algos.pg.a2c import A2C
+from rlpyt.agents.pg.categorical import CategoricalPgAgent
+
+def f(*args, **kwargs):
+    return GymEnvWrapper(DeepSeaTreasureEnv())
+
+def build_and_train(run_nr = 0):
+    sample = SerialSampler(
+        EnvCls=f,
+        env_kwargs={},
+        batch_T=50,
+        batch_B=10,
+        max_decorrelation_steps=0,
+        eval_n_envs=10,
+        eval_max_steps=10e3,
+        # eval_max_trajectories=5,
+        eval_env_kwargs={}
+    )
+
+    algo = A2C() #default values
+    spaces = namedtuple('spaces', ['actionspace', 'observationspace'])
+    agent = CategoricalPgAgent(
+                                ModelCls=CatMlpPgModel,
+                                model_kwargs = {'input_size':3,
+                                                'hidden_sizes': 20,
+                                                "output_size": 4})
+
+    runner = MinibatchRlEval(
+        algo = algo,
+        agent= agent,
+        sampler = sample,
+        n_steps = 40e6,
+        log_interval_steps = 10e3
+    )
+    config = {}
+    name = "deep_sea_treasure_dqn: " + DeepSeaTreasureEnv.__class__.__name__
+    log_dir = "deep_sea_treasure_env"
+    with logger_context(log_dir, run_nr, name, config, snapshot_mode='last', use_summary_writer=True):
+        runner.train()
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--run', help='run number (for logging)', type=int, default=0)
+    args = parser.parse_args()
+
+    build_and_train(
+        run_nr= args.run
+    )
